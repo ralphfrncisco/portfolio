2cb941edac7f3612973a675f850b41e8fa0361b1
ralphfrncisco <ralphfrncisco@gmail.com>
2025-11-06 01:04:31 +0800

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const navbar1 = document.getElementById('navbar');
+  console.log(navbar1)
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      navbar1.classList.add('scrolled');
+    } else {
+      navbar1.classList.remove('scrolled');
+    }
+  });
+});
