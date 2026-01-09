0d456da077c9a95fa134d74ee334157a435c375e
ralphfrncisco <ralphfrncisco@gmail.com>
2026-01-09 17:55:13 +0800

    added fade up animation on load.
    AOS library.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import './App.css';
 
 import Header from './layout/Header.jsx';
@@ -11,6 +13,18 @@ import Projects from './layout/Projects.jsx';
 function App() {
   const [darkMode, setDarkMode] = useState(true)
 
+  // AOS comment for future changes
+
+  useEffect(() => {
+    AOS.init({
+      duration: 400,       // animation duration in milliseconds
+      once: false,         // whether animation should happen only once
+      offset: 3,           // offset (in px) from the original trigger point
+      easing: 'ease',      // animation easing
+      delay: 50           // delay before the animation starts
+    });
+  }, []);
+
   return (
     <div className={`min-h-screen w-full antialiased transition-colors duration-300
     ${darkMode ? 'portfolio-gradient text-white' : 'bg-gray-50 text-gray-900'}`}>
@@ -20,27 +34,33 @@ function App() {
         </section>
 
         <main>
-          <section className = "grid grid-cols-1 md:grid-cols-3">
-            <div className="col-span-1 md:col-span-2">
+          <section className="grid grid-cols-1 md:grid-cols-3">
+            <div className="col-span-1 md:col-span-2" data-aos="fade-up">
               <About darkMode={darkMode} />
             </div>
 
-            <div className="col-span-1 md:col-span-1">
+            <div className="col-span-1 md:col-span-1" data-aos="fade-up">
               <Experience darkMode={darkMode} />
             </div>
           </section>
           
-          <section className = "grid grid-cols-1 md:grid-cols-2">
-            <TechStack darkMode={darkMode} />
-            <Certifications darkMode={darkMode} />
+          <section className="grid grid-cols-1 md:grid-cols-2">
+            <div data-aos="fade-up">
+              <TechStack darkMode={darkMode} />
+            </div>
+            <div data-aos="fade-up">
+              <Certifications darkMode={darkMode} />
+            </div>
           </section>
 
-          <Projects darkMode={darkMode} />
+          <div data-aos="fade-up">
+            <Projects darkMode={darkMode} />
+          </div>
         </main>
       </div>
 
       <footer>
-        <div className= {`mx-auto pt-6 pb-6 p-4 text-center text-xs font-light ${darkMode ? 'text-white/60' : 'text-gray-500'}`}>
+        <div className={`mx-auto pt-6 pb-6 p-4 text-center text-xs font-light ${darkMode ? 'text-white/60' : 'text-gray-500'}`}>
           <p>© 2024 Ralph Angelo Francisco. All rights reserved.</p>
         </div>
       </footer>
