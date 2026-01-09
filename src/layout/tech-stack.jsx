d53560dee285850b5a73d854b73fdb870cf0de71
ralphfrncisco <ralphfrncisco@gmail.com>
2026-01-09 18:08:09 +0800

    fixed padding issues tech-stack layout component.

--- src/layout/tech-stack.jsx
+++ src/layout/tech-stack.jsx
@@ -56,7 +56,7 @@ function TechStack({darkMode}) {
 
   return (
     <section className="p-2">
-        <div className={`flex items-center border rounded-lg p-4 pb-10 transition-colors duration-300 ${
+        <div className={`flex items-center border rounded-lg p-4 md:pb-10 transition-colors duration-300 ${
             darkMode ? 'bg-[#111] border-[#222] text-white' : 'bg-white border-gray-200 text-gray-800 shadow-sm'
         }`}>
             <div className="flex-1 min-w-0">
